c77eb05c7451f1e2c4e6d446a15a56953f382d85
Jesús Sierra <dev.jesus.sierra@gmail.com>
2021-12-26 10:59:06 -0500

    Completada sección 2.12

---
+++ src/bases/CounterBy.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+interface Props {
+  initialValue?: number
+}
+
+export const CounterBy = ({ initialValue = 5 }: Props) => {
+  const [counterState, setCounterState] = useState({
+    counter: initialValue,
+    clicks: 0
+  })
+  const handleClick = (value: number) => {
+    setCounterState(prev => ({ counter: prev.counter + value, clicks: prev.clicks + 1 }))
+  }
+
+  return (
+    <>
+      <h1>CounterBy: {counterState.counter}</h1>
+      <h1>Clicks: {counterState.clicks}</h1>
+
+      <button onClick={() => handleClick(1)}>+1</button>
+      <button onClick={() => handleClick(5)}>+5</button>
+    </>
+  )
+}
