0b2af2efd47b87e47291cec515f289d3aa5d576e
Jesús Sierra <dev.jesus.sierra@gmail.com>
2021-12-26 15:37:28 -0500

    Completada sección 2.16

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { gsap } from 'gsap'
 
 const MAXIMUN_COUNT = 10
 
 export const CounterEffect = () => {
   const [counter, setCounter] = useState(5)
+
+  const counterElement = useRef<HTMLHeadingElement>(null)
+  
   const handleClick = () => {
     setCounter(counter => Math.min(counter + 1, MAXIMUN_COUNT))
   }
@@ -13,16 +16,16 @@ export const CounterEffect = () => {
     if (counter < 10) return;
     
     console.log('%cSe llegó al valor máximo.', 'color: red; background-color: blue;')
-    gsap.to('h2', { y: -10, duration: 0.2, ease: 'ease.out' }).then(() => {
-      gsap.to('h2', { y: 10, duration: 1, ease: 'bounce.out' })
-    })
+    const timeLine = gsap.timeline()
+    timeLine.to(counterElement.current, { y: -10, duration: 0.2, ease: 'ease.out' })
+      .to(counterElement.current, { y: 10, duration: 1, ease: 'bounce.out' })
       
   }, [counter])
 
   return (
     <>
       <h1>CounterEffect:</h1>
-      <h2>{counter}</h2>
+      <h2 ref={counterElement}>{counter}</h2>
       <button onClick={handleClick}>
         +1
       </button>
